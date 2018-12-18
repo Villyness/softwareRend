cafd24911001bc4f72360f116f1f87b84bf9055e
Venus <inkyinked@gmail.com>
2018-12-18 13:29:23 +1100

    Doesn't seem to be accessing the meshes in the demo script.

---
+++ Assets/Scripts/1/demo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+    public int ClusterAmount;
+    public List<mesh> Cluster = new List<mesh>();
+
+	// Use this for initialization
+	void Start ()
+	{
+	    for (int i = 0; i < ClusterAmount; i++)
+	    {
+	        mesh group = new mesh();
+            Cluster.Add(group);
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
--- Assets/Scripts/1/rend.cs
+++ Assets/Scripts/1/rend.cs
@@ -2,30 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rend : MonoBehaviour       // Just need to replace star with vertices
+public class rend : MonoBehaviour // Just need to replace star with vertices
 {
     public int xSize;
     public int ySize;
-    
+
     public int Red;
     public int Green;
     public int Blue;
 
     public MeshRenderer quadRenderer;
     public byte[] backBuffer;
-    public Texture2D tex;
-    public List<Star> Nova = new List<Star>();
-    public Vector3 Pos;
 
-    public bool Clear;
+    public Texture2D tex;
+    //public List<Star> Nova = new List<Star>();
 
     private int ByteSize;
 
     // Mesh stuff
-    public List<mesh> CubeGroup;
+    public List<mesh> CubeGroup = new List<mesh>();
+    public List<vertex> meshVertex;
+    public GameObject testObject;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         ByteSize = xSize * ySize * 3;
         backBuffer = new byte[ByteSize];
@@ -34,29 +34,65 @@ public class rend : MonoBehaviour       // Just need to replace star with vertic
         tex.filterMode = FilterMode.Point; // Removes blurring
         quadRenderer.material.mainTexture = tex;
 
-        for (int i = 0; i < 10; i++)
+        /*for (int i = 0; i < 10; i++)
         {
-            Star _star = new Star();
+            Star star = new Star();
             Pos = new Vector3(Random.Range(0, 16), Random.Range(0, 16), 0);
             _star.OwnPos = Pos;
             Nova.Add(_star);
         }
+        for(int i = 0; i < 10; i++)
+        {
+            mesh group = new mesh();
+            CubeGroup.Add(group);
+        }*/
+        foreach (mesh cube in GetComponent<demo>().Cluster)
+        {
+            Debug.Log("Hello World!");
+            CubeGroup.Add(cube);
+            foreach (vertex corner in cube.Vertices)
+            {
+                meshVertex.Add(corner);
+            }
+        }
     }
-	
-	// Update is called once per frame
-	void Update ()
-	{
+
+    // Update is called once per frame
+    void Update()
+    {
         ColourChange(0, 0, 0);
 
-        foreach(Star star in Nova)
+        foreach (mesh cluster in CubeGroup)
         {
-            PixelChange((int)(((star.OwnPos.x % 16 + (star.OwnPos.x)) / (star.OwnPos.z))/2), (int)(((star.OwnPos.y % 16 + (star.OwnPos.y)) / (star.OwnPos.z))/2), 255, 255, 255);   // Call the function to draw the stars
+            Debug.Log("Hello World");
+            foreach (vertex corner in meshVertex)
+            {
+                Instantiate(testObject, corner.OwnPos, Quaternion.identity);
+            }
+            if ((cluster.OwnPos.x) / (cluster.OwnPos.z) < 0) // If x/z were to give a negative value (and hence break the PixelChange()), then set it to positive/negative
+            {
+                cluster.OwnPos.x = (cluster.OwnPos.x) * (-1);
+            }
+
+            if ((cluster.OwnPos.y) / (cluster.OwnPos.z) < 0)
+            {
+                cluster.OwnPos.y = (cluster.OwnPos.y) * (-1);
+            }
+
+            if (((cluster.OwnPos.x % 16) / (cluster.OwnPos.z)) < 8 /*&& ((star.OwnPos.x % 16) / (star.OwnPos.z)) > 0*/
+            ) // Reset the location after it goes below 1
+            {
+                Vector3 NewPos = new Vector3(Random.Range(8, 16), Random.Range(8, 16), -1);
+                cluster.OwnPos = NewPos;
+            }
+
+            PixelChange((int) (((cluster.OwnPos.x % 16 + (cluster.OwnPos.x)) / (cluster.OwnPos.z)) / 2),
+                (int) (((cluster.OwnPos.y % 16 + (cluster.OwnPos.y)) / (cluster.OwnPos.z)) / 2), 255, 255,
+                255); // Call the function to draw the stars
         }
 
         tex.LoadRawTextureData(backBuffer);
         tex.Apply(false);
-
-        SpawnCubes();
     }
 
     public void ColourChange(int R, int G, int B)
@@ -69,48 +105,27 @@ public class rend : MonoBehaviour       // Just need to replace star with vertic
 
     public void PixelChange(int x, int y, int R, int G, int B)
     {
-        foreach (Star star in Nova)
-        {
-            star.Start();   // This sets z to -1; runs only once
+        /*if (((star.OwnPos.y % 16) / (star.OwnPos.z)) < 1 && ((star.OwnPos.y % 16) / (star.OwnPos.z)) > 0)
+{
+    Vector3 NewPos = new Vector3(Random.Range(0, 16), Random.Range(0, 16), -1);
+    star.OwnPos = NewPos;
+}
 
-            if ((star.OwnPos.x) / (star.OwnPos.z) < 0) // If x/z were to give a negative value (and hence break the PixelChange()), then set it to positive/negative
-            {
-                star.OwnPos.x = (star.OwnPos.x) * (-1);
-            }
-
-            if ((star.OwnPos.y) / (star.OwnPos.z) < 0)
-            {
-                star.OwnPos.y = (star.OwnPos.y) * (-1);
-            }
-
-            if (((star.OwnPos.x % 16) / (star.OwnPos.z)) < 8 /*&& ((star.OwnPos.x % 16) / (star.OwnPos.z)) > 0*/)   // Reset the location after it goes below 1
-            {
-                Vector3 NewPos = new Vector3(Random.Range(8, 16), Random.Range(8, 16), -1);
-                star.OwnPos = NewPos;
-            }
-
-            if (((star.OwnPos.y % 16) / (star.OwnPos.z)) < 1 && ((star.OwnPos.y % 16) / (star.OwnPos.z)) > 0)
-            {
-                Vector3 NewPos = new Vector3(Random.Range(0, 16), Random.Range(0, 16), -1);
-                star.OwnPos = NewPos;
-            }
-
-            star.Update();
-            Debug.Log((star.OwnPos.x % 16) / (star.OwnPos.z));
-        }
+star.Update();
+Debug.Log((star.OwnPos.x % 16) / (star.OwnPos.z));*/
 
         // Need to refactor it to take cubes
-        backBuffer[(xSize * 3 * y) + (x * 3)] = (byte)R;
-        backBuffer[(xSize * 3 * y) + ((x * 3) + 1)] = (byte)G;
-        backBuffer[(xSize * 3 * y) + ((x * 3) + 2)] = (byte)B;
+        backBuffer[(xSize * 3 * y) + (x * 3)] = (byte) R;
+        backBuffer[(xSize * 3 * y) + ((x * 3) + 1)] = (byte) G;
+        backBuffer[(xSize * 3 * y) + ((x * 3) + 2)] = (byte) B;
     }
 
-    public void SpawnCubes()
+    /*public void SpawnCubes()
     {
         for (i = 0; i < 4; i++)
         {
             mesh cubeCluster = new mesh();
 
         }
-    }
+    }*/
 }
